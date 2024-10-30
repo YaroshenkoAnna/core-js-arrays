cf846540c7f7c14210c19812479ca429c0f7ba84
Anna <yaroshenkoannette@gmail.com>
2024-10-30 10:41:33 +0100

    feat: add createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -342,8 +342,18 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const res = [];
+  function iteration(array) {
+    let elem;
+    if (array.length > chunkSize) {
+      elem = array.splice(0, chunkSize);
+      res.push(elem);
+    } else return array;
+    return iteration(array);
+  }
+  res.push(iteration(arr));
+  return res;
 }
 
 /**
